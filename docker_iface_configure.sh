f2986a0bb4712e32f21229102871a745bbf05cff
Chaitanya Lala <chaitanyalala@users.noreply.github.com>
2018-10-04 22:39:17 -0700

    Add files via upload
    Initial commit

---
+++ docker_iface_configure.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ifconfig -a 
+ifconfig eth0 
+ifconfig eth0 0
+ifconfig eth0 
+ifconfig eth1 0
+ifconfig -a 
+ethtool -i eth0
+ethtool -i eth1
+brctl addbr snoopbr
+brctl snoopbr addif eth0
+brctl addif snoopbr eth0
+brctl addif snoopbr eth1
+brctl show 
+ifconfig -a 
+ifconfig snoopbr 0
+ifconfig -a 
+ifconfig -a 
