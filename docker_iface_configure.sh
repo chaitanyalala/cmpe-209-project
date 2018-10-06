6e7d47ace0fc86051cd5cb010ae4537571bd061f
Chaitanya Lala <chaitanya.lala@gmail.com>
2018-10-06 00:16:35 +0000

    make rules file work

--- docker_iface_configure.sh
+++ docker_iface_configure.sh
@@ -7,6 +7,7 @@ brctl addif snoopbr eth0
 brctl addif snoopbr eth1
 ifconfig snoopbr 0
 echo 'alert tcp any 5001 -> any any (msg:"iperf test";)' > /etc/suricata/rules/local.rules
+sed -i s/" # - botcc.portgrouped.rules"/" - local.rules"/g /etc/suricata/suricata.yaml
 service suricata start
 # Don't let the script exit else it would cause the container to exit.
 /bin/bash
